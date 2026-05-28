bf205506b78f40f0b414fe7687b9ffa1d37a56f9
Dave Falls <288647190+GCITdave@users.noreply.github.com>
2026-05-28 16:59:50 -0400

    add web3forms contact form

--- my-tina-app/tina/config.ts
+++ my-tina-app/tina/config.ts
@@ -4,11 +4,7 @@ import { GlobalConfigCollection } from "./collections/global-config";
 import { PageCollection } from "./collections/page";
 
 // Your hosting provider likely exposes this as an environment variable
-const branch =
-  process.env.GITHUB_BRANCH ||
-  process.env.VERCEL_GIT_COMMIT_REF ||
-  process.env.HEAD ||
-  "main";
+const branch = "main";
 
 export default defineConfig({
   branch,
